0bbbfc0e6dfe1673abec041be53cd6e597830ec0
mcastroif <caastro24@gmail.com>
2025-07-20 18:12:59 -0300

    On branch main
     Initial commit

---
+++ 2(basico)/Program.cs
@@ -0,0 +1,8 @@
+﻿using Basico.Models; //chamar o namespace Basico.Models definido na classe Pessoa
+
+Pessoa p = new Pessoa(); //utiliza a classe Pessoa para definir um objeto p
+p.Nome = "João";
+p.Idade = 30;
+p.Apresentar(); //metodo apresentar da classe Pessoa
+
+
