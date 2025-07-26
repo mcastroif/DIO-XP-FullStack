b2071f9c8715e8b1eac73a1ea63af0a7b32c4b0b
mcastroif <caastro24@gmail.com>
2025-07-26 19:26:54 -0300

    Commit criando pasta da aula arrays

---
+++ 2/2.4-Arrays/Arrays-Listas.cs
@@ -0,0 +1,7 @@
+﻿int[] array = new int[5]; // declarando um array com no maximo 5 elementos
+
+int[] array2 = new int[] { 1, 2, 3, 4, 5 }; // declarando e inicializando um array com 5 elementos determinados
+
+int[] array3 = { 1, 2, 3, 4, 5 }; // declarando e inicializando um array com 5 elementos determinados
+
+//pode-se omitir o new int[] na inicialização do array, mas é necessário informar os valores entre chaves
